358dae9e5477482f82a967b44848e80cc21b6161
Emily Wozniak <ewozniak4623@gmail.com>
2025-06-21 18:27:42 -0400

    Add contact form to Contact page and style it for better user interaction

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-
+fetch("https://ipapi.co/json/")
+  .then(response => response.json())
+  .then(data => {
+    if (data.country === "NL") {
+      alert("Sorry Flora, No access to my Ken Rockwell clone website!");
+      window.location.href = "https://google.com";
+    }
+  });
